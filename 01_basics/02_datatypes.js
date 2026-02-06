f6a97b83480687ccf893aaf8184cca2ff5828e0c
xhirag <141368498+xhirag@users.noreply.github.com>
2026-02-06 11:29:08 +0530

    Datatypes JS

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict" // treat all the JS cose as newer version
+
+// alert("hello");  // we are using nodejs , not browser
+
+// number 
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone *value*
+// undefined => no value 
+// symbol => uniqueness (used in react figma)
+// object
+
+console.log(typeof undefined);  //undefined 
+console.log(typeof null);    //object
+
+
